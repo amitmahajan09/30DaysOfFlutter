da05bf78a330fe222c4e20aa4ce1e704f50ac71b
Amit Mahajan <amitku.mahajan@gmail.com>
2021-06-15 15:22:04 +0530

    Commit on day3

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,16 +4,13 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        child: Column(
+      children: [
+        Image.asset(
+          "assets/images/login_image.png",
+          fit: BoxFit.cover,
+        )
+      ],
+    ));
   }
 }
