975ecbe47236de9af46081fd5c0697e7459d15d0
Amit Mahajan <amitku.mahajan@gmail.com>
2021-06-18 21:04:53 +0530

    Day9 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
@@ -14,7 +15,7 @@ class HomePage extends StatelessWidget {
           child: Text("Welcome to $days days of Flutter Course by $name"),
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
