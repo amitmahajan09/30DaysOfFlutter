5959adbdb86a6be4abab72096379a9bc73cc5641
Amit Mahajan <amitku.mahajan@gmail.com>
2021-06-19 17:25:19 +0530

    themes extracted

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_complete_guide/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
-  final String name = "Codepur";
+  final String name = "Amit";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
