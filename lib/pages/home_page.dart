b2bbbd5e3ee9d101fb2a030f4e94165d7774933d
Amit Mahajan <amitku.mahajan@gmail.com>
2021-06-20 18:45:03 +0530

    day13 list item completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_complete_guide/models/catalog.dart';
 import 'package:flutter_complete_guide/widgets/drawer.dart';
 import 'package:flutter_complete_guide/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Amit";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    // print(decodeedData);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {}); //to load complete list and it call build method again
+    // print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    //   final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Center(child: Text("Catalog App")),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-            itemCount: dummyList.length,
+            //    itemCount: dummyList.length,
+            itemCount: CatalogModel.items.length,
             itemBuilder: (context, index) {
               return ItemWidget(
-                item: dummyList[index],
+                //        item: dummyList[index],
+                item: CatalogModel.items[index],
               );
             }),
       ),
