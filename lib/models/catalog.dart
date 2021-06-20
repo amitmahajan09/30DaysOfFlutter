7cb99e51db130e47bc87b7d41765e7e44be83c7a
Amit Mahajan <amitku.mahajan@gmail.com>
2021-06-20 16:03:51 +0530

    day12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,19 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: 'Iphone12',
+      desc: 'Iphone 12th Generation',
+      price: 999,
+      color: '#33505',
+      image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPJKV6tazOeNkQ9aYllOnfWKUves9Py6zdGrTGjflLB6S5iZr4FS9qSaMAFJznmeQ43Y2Wlws&usqp=CAc',
+    )
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -14,15 +28,3 @@ class Item {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Item(
-    id: 'Amit',
-    name: 'Iphone12',
-    desc: 'Iphone 12th Generation',
-    price: 70000,
-    color: '#33505',
-    image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPJKV6tazOeNkQ9aYllOnfWKUves9Py6zdGrTGjflLB6S5iZr4FS9qSaMAFJznmeQ43Y2Wlws&usqp=CAc',
-  )
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/catalog.dart';
 import 'package:flutter_complete_guide/widgets/drawer.dart';
+import 'package:flutter_complete_guide/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Amit";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of Flutter Course by $name"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            }),
       ),
       drawer: MyDrawer(),
     );
